2a233fd760a924d83bfc82f896b3efebf8f56043
Mansi Zope <mansizope@Mansis-MacBook-Pro.local>
2018-08-29 21:05:20 +0530

    initial commit

---
+++ I'm RIch AppTests/I_m_RIch_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  I_m_RIch_AppTests.swift
+//  I'm RIch AppTests
+//
+//  Created by Mansi Zope on 26/08/18.
+//  Copyright © 2018 Mansi Zope. All rights reserved.
+//
+
+import XCTest
+@testable import I_m_RIch_App
+
+class I_m_RIch_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
